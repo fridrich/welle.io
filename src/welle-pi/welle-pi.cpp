9c9953fbd6eab14091d87dedff22f1863d1fd873
Fridrich Strba <fridrich.strba@bluewin.ch>
2026-08-12 19:58:57 +0200

    Implement DAB+ auto-scanner logic (welle-pi)

--- src/welle-pi/welle-pi.cpp
+++ src/welle-pi/welle-pi.cpp
@@ -1349,6 +1349,66 @@ static bool tune_to_service(RadioReceiver& rx, AlsaProgrammeHandler& ph,
     return service_selected;
 }
 
+static void runAutoScanner(RadioReceiver& rx, CVirtualInput* in, RadioInterface& ri, Channels& channels, ConfigManager& config, UIManager& uiManager) {
+    auto scanningScreen = make_shared<ScanningScreen>();
+    uiManager.setScreen(scanningScreen);
+
+    const std::vector<std::string> SCAN_CHANNELS = {
+        "5A", "5B", "5C", "5D",
+        "6A", "6B", "6C", "6D",
+        "7A", "7B", "7C", "7D",
+        "8A", "8B", "8C", "8D",
+        "9A", "9B", "9C", "9D",
+        "10A", "10B", "10C", "10D",
+        "11A", "11B", "11C", "11D",
+        "12A", "12B", "12C", "12D",
+        "13A", "13B", "13C", "13D", "13E", "13F"
+    };
+
+    config.clearStations();
+
+    for (const auto& channel : SCAN_CHANNELS) {
+        if (stop_requested()) break;
+
+        int foundCount = config.getStations().size();
+        scanningScreen->setScanningStatus(channel, foundCount);
+
+        auto freq = channels.getFrequency(channel);
+        in->setFrequency(freq);
+        rx.restart(false);
+        ri.resetSyncWatchdog();
+        ri.synced = false;
+
+        int sync_wait = 30;
+        while (!ri.synced && sync_wait > 0 && !stop_requested()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            sync_wait--;
+        }
+
+        if (ri.synced) {
+            int service_wait = 50;
+            while (rx.getServiceList().empty() && service_wait > 0 && !stop_requested()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                service_wait--;
+            }
+            if (!rx.getServiceList().empty() && !stop_requested()) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                for (const auto& s : rx.getServiceList()) {
+                    ConfigManager::Station station;
+                    station.channel = channel;
+                    station.program = s.serviceLabel.utf8_label();
+                    station.service_id = s.serviceId;
+                    config.addStation(station);
+                }
+            }
+        }
+    }
+
+    if (!stop_requested()) {
+        config.saveConfig();
+    }
+}
+
 int main(int argc, char **argv)
 {
     auto options = parse_cmdline(argc, argv);
